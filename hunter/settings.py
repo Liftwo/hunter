b13ebd6bc8ad09e4ed50efc5eb9553399d962b44
zooeytsai <s1301h030@gmail.com>
2021-07-28 01:25:34 +0800

    delete sk

--- hunter/settings.py
+++ hunter/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'fexil+ln!o!8ioq=ds-bsc$%bx6c#)r-_$w6h7*h0_9@mmqw08'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
